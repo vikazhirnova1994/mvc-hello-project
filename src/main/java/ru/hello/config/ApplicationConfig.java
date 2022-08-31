7f7e4c177b307f3121360e470cba508d5505249b
Vika Znirnova <vika.znirn@yandex.ru>
2022-08-31 18:15:51 +0500

    Adding styles for templates

--- src/main/java/ru/hello/config/ApplicationConfig.java
+++ src/main/java/ru/hello/config/ApplicationConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
@@ -58,5 +59,14 @@ public class ApplicationConfig implements WebMvcConfigurer {
         resolver.setTemplateEngine(templateEngine());
         registry.viewResolver(resolver);
     }
+
+ /*   @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        // Css resource.
+        registry.addResourceHandler("/styles/**") //
+                .addResourceLocations("/WEB-INF/static/css/");
+
+    }*/
 
 }
